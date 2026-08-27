a2b4e9c5e45d9b32bc82585e8405434c4ca3188f
Trafina Catherine <78035562+shiny477@users.noreply.github.com>
2026-08-27 13:28:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_5WocX11Ts
-', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '56l19gkffn7co3711n527sinbm
-', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'Asia Pacific' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_5WocX11Ts', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '56l19gkffn7co3711n527sinbm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
